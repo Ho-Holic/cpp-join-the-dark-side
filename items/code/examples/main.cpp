d5214cf7c917c1f64078b492e143d80de1d46f5e
Igor Sysoev <isysoev@zeptolab.com>
2018-02-12 18:33:13 +0300

    refactor to new funcy name

--- items/code/examples/main.cpp
+++ items/code/examples/main.cpp
@@ -4,14 +4,14 @@
 #include <cassert>
 
 
-
+// proxy for retrieving result
 template <typename... Ts>
-struct LinkedTypesGetter  {
+struct DimensionMapResult  {
 };
 
 template <typename C>
-struct LinkedTypesGetter<C>  {
-    LinkedTypesGetter(const C& data) : m_data(data) {}
+struct DimensionMapResult<C>  {
+    DimensionMapResult(const C& data) : m_data(data) {}
 
     template <typename T>
     T get() {
@@ -23,28 +23,37 @@ struct LinkedTypesGetter<C>  {
 
 
 template <typename C, typename U1, typename... Us>
-struct LinkedTypesGetter<C, U1, Us...> : LinkedTypesGetter<C, Us...>  {
+struct DimensionMapResult<C, U1, Us...> : DimensionMapResult<C, Us...>  {
 
-    using Base = LinkedTypesGetter<C, Us...>;
-    using Base::m_data;
-    //using Base::operator Us...; // c++17
+    using Base = DimensionMapResult<C, Us...>;
+    using Base::m_data;    
 
-    LinkedTypesGetter(const C& data) : LinkedTypesGetter<C, Us...>(data) {}
+    DimensionMapResult(const C& data) : DimensionMapResult<C, Us...>(data) {}
 
     operator U1() {
         return std::get<U1>(m_data);
     }
 };
 
+// trait
 template <size_t N, typename... Ts>
-struct LinkedTypes  {
+struct DimensionTrait
+{
+    enum : size_t { dimensions = N };
+    using tuple_type = std::tuple<Ts...>;
+    using result_type = DimensionMapResult<tuple_type, Ts...>;
+};
+
+// main container
+template <typename... Ts>
+struct DimensionMap  {
     //
 };
 
-template <size_t N, typename C, typename G>
-struct LinkedTypes<N, C, G> {
-    LinkedTypes(const C (&c)[N]) {
-        for (size_t i = 0; i < N; ++i) {
+template <typename DT>
+struct DimensionMap<DT> {
+    DimensionMap(const typename DT::tuple_type (&c)[DT::dimensions]) {
+        for (size_t i = 0; i < DT::dimensions; ++i) {
             m_storage[i] = c[i];
         }
     }
@@ -53,38 +62,86 @@ struct LinkedTypes<N, C, G> {
         // to make 'using Base::operator();' work
     }
 
-    C m_storage[N];
+    typename DT::tuple_type m_storage[DT::dimensions];
 };
 
-template <size_t N, typename C, typename G, typename U1, typename... Us>
-struct LinkedTypes<N, C, G, U1, Us...> : LinkedTypes<N, C, G, Us...> {
+// version with skip
 
-    using Base = LinkedTypes<N, C, G, Us...>;
+template<class T, class EqualTo>
+struct has_operator_less_detail
+{
+    template<class U, class V>
+    static auto test(U*) -> decltype(std::declval<U>() < std::declval<V>());
+    template<typename, typename>
+    static auto test(...) -> std::false_type;
+
+    using type = typename std::is_same<bool, decltype(test<T, EqualTo>(0))>::type;
+};
+
+template<class T, class EqualTo = T>
+struct has_operator_less : has_operator_less_detail<T, EqualTo>::type {};
+
+template <typename T, bool>
+struct Chooser {
+    using value = void;
+};
+
+template <typename T>
+struct Chooser<T, true> {
+    using value = T;
+};
+
+// version with getter
+template <typename DT, typename U1, typename... Us>
+struct DimensionMap<DT, U1, Us...> : DimensionMap<DT, Us...> {
+
+    using Base = DimensionMap<DT, Us...>;
     using Base::m_storage;
     using Base::operator();
 
-    LinkedTypes(const C (&c)[N]) : LinkedTypes<N, C, G, Us...>(c) {
-        for (size_t i = 0; i < N; ++i) {
+    DimensionMap(const typename DT::tuple_type (&c)[DT::dimensions]) : DimensionMap<DT, Us...>(c) {
+        for (size_t i = 0; i < DT::dimensions; ++i) {
             m_layer.insert(std::make_pair(std::get<U1>(c[i]), i));
         }
     }
 
-    G operator()(const U1& key) {
+    typename DT::result_type operator()(const U1& key) {
         auto found = m_layer.find(key);
         assert(found != m_layer.end());
 
         auto tupleRow = m_storage[found->second];
-        return G(tupleRow);
+        return typename DT::result_type(tupleRow);
     }
+
+    template <typename Any>
+    typename DT::result_type operator()(const U1& key, const Any& fallback) {
+        auto found = m_layer.find(key);
+        return found != m_layer.end() ? m_storage[found->second] : G(fallback);
+    }
+
     std::map<U1, size_t> m_layer;
 };
 
+//template <typename DT, typename U1, typename... Us>
+//struct DimensionMap<DT, void, Us...>
+//:   DimensionMap<DT, Us...> {
+
+//    using Base = DimensionMap<DT, Us...>;
+//    using Base::m_storage;
+//    using Base::operator();
+
+//    DimensionMap(const C (&c)[DT::dimensions]) : DimensionMap<DT, Us...>(c) {
+//        //
+//    }
+//};
+
 template <typename... Ts, size_t N>
-auto makeLinkedTypes(const std::tuple<Ts...>(&items)[N])
--> LinkedTypes<N, std::tuple<Ts...>, LinkedTypesGetter<std::tuple<Ts...>, Ts...>, Ts...> {
-    LinkedTypes<N, std::tuple<Ts...>, LinkedTypesGetter<std::tuple<Ts...>, Ts...>, Ts...> d ( items );
+auto makeDimensionMap(const std::tuple<Ts...>(&items)[N]) -> DimensionMap<DimensionTrait<N, Ts...>, Ts...> {
+    DimensionMap<DimensionTrait<N, Ts...>, Ts...> d ( items );
     return d;
 }
+
+
 
 // client
 
@@ -105,23 +162,23 @@ bool operator< (const Point& left, const Point& right) {
 int main() {
 
 
-    auto data = makeLinkedTypes<std::string, int, Point>({
+    auto data = makeDimensionMap<std::string, int, Point>({
       {"cat", 42, {42, 12}},
       {"dog", 12, {1, 1}},
       {"parrot", 13, {8, 16}},
     });
 
-    {
-        auto g = data(Point(42, 12));
-        std::string s = g;
-        int i = g;
-        Point p = g;
+//    {
+//        auto g = data(Point(42, 12));
+//        std::string s = g;
+//        int i = g;
+//        Point p = g;
 
-        std::cout << ">Cat" << std::endl;
-        std::cout << "String: " <<  s << std::endl;
-        std::cout << "Integer: " <<  i << std::endl;
-        std::cout << "Point: " <<  p.x << "," << p.y << std::endl;
-    }
+//        std::cout << ">Cat" << std::endl;
+//        std::cout << "String: " <<  s << std::endl;
+//        std::cout << "Integer: " <<  i << std::endl;
+//        std::cout << "Point: " <<  p.x << "," << p.y << std::endl;
+//    }
 
     {
         auto g = data(12);
