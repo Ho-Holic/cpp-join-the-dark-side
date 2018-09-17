6221df9d2d7fcf6d7694e5320c173785ce7b7dff
Igor Sysoev <isysoev@zeptolab.com>
2018-09-17 10:42:01 +0300

    sketch of cache

--- items/code/examples/main.cpp
+++ items/code/examples/main.cpp
@@ -1,4 +1,90 @@
 #include <iostream>
+#include <functional>
+#include <map>
+#include <vector>
+
+namespace SA
+{
+    template <typename T>
+    class Cache
+    {
+    public:
+        using cache_type = std::vector<T>;
+        using callback = std::function<void(cache_type&)>;
+
+    public:
+        Cache();
+        const cache_type& getOrFill(callback&& writeTo);
+        void invalidate();
+
+    private:
+        bool m_isCached;
+        cache_type m_cachedValues;
+    };
+
+    template <typename K, typename V>
+    class CacheMap
+    {
+    public:
+        using cache_type = std::map<K, V>;
+        using callback = std::function<void(cache_type&)>;
+
+    public:
+        CacheMap();
+        const cache_type& getOrFill(callback&& writeTo);
+        void invalidate();
+
+    private:
+        bool m_isCached;
+        cache_type m_cachedValues;
+    };
+
+    template <typename T>
+    Cache<T>::Cache()
+        : m_isCached(false)
+    {
+    }
+
+    template <typename K, typename V>
+    CacheMap<K, V>::CacheMap()
+        : m_isCached(false)
+    {
+    }
+
+    template <typename T>
+    const typename Cache<T>::cache_type& Cache<T>::getOrFill(callback&& writeTo)
+    {
+        if (!m_isCached) {
+            writeTo(m_cachedValues);
+            m_isCached = true;
+        }
+        return m_cachedValues;
+    }
+
+    template <typename K, typename V>
+    const typename CacheMap<K, V>::cache_type& CacheMap<K, V>::getOrFill(callback&& writeTo)
+    {
+        if (!m_isCached) {
+            writeTo(m_cachedValues);
+            m_isCached = true;
+        }
+        return m_cachedValues;
+    }
+
+    template <typename T>
+    void Cache<T>::invalidate()
+    {
+        m_isCached = false;
+        m_cachedValues.clear();
+    }
+
+    template <typename K, typename V>
+    void CacheMap<K, V>::invalidate()
+    {
+        m_isCached = false;
+        m_cachedValues.clear();
+    }
+}
 
 template <typename T>
 struct TreeGen
@@ -28,8 +114,20 @@ TreeGenItem<T> operator-(const T& value)
 }
 
 // client
+//  root +=
+//      - node1,
+//      - - subnode1,
+//      - - subnode2,
+//      - node2,
+//      - node3,
+//      - node4;
+
+root += node1, --subnode1, --subnode2, -node2, -node3, -node4;
 
 int main() {
+
+    std::cout << "Main Darkside:" << std::endl;
+
     TreeGen<std::string> root;
 
 //    root +=
