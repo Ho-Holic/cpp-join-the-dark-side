51aa9bc4c61bd76b32546fbdf1fc9ea4f8866097
Igor Sysoev <isysoev@zeptolab.com>
2018-10-16 14:12:26 +0300

    add random traits

--- items/code/examples/include/Random.hpp
+++ items/code/examples/include/Random.hpp
@@ -9,7 +9,25 @@
 #include <initializer_list>
 
 
-class Random
+struct FastRandomTraits
+{
+    using GeneratorType = std::mt19937;
+    static GeneratorType& generator();
+};
+
+//
+// if you want one generator for all translation units, please use custom traits
+//
+inline std::mt19937& FastRandomTraits::generator()
+{
+    static std::random_device s_device;
+    static std::mt19937 s_fastGenerator(s_device());
+    return s_fastGenerator;
+}
+
+
+template <typename RandomTraits>
+class RandomBase
 {
 public:
     template <typename T>
@@ -46,23 +64,21 @@ public:
 
     template <typename T>
     static T triangularf(T a, T b, T c);
-
-private:
-    static std::random_device& randomDevice();
 };
 
 
 // implementation
 
+template <typename RandomTraits>
 template <typename T>
-T Random::triangularf(T a, T b, T c)
+T RandomBase<RandomTraits>::triangularf(T a, T b, T c)
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
 
     //
     // https://en.wikipedia.org/wiki/Triangular_distribution#Generating_triangular-distributed_random_variates
     //
-    const auto u = Random::uniformf<T>();
+    const auto u = RandomBase::uniformf<T>();
     const auto f = (c - a) / (b - a);
 
     if (u < f) {
@@ -72,63 +88,71 @@ T Random::triangularf(T a, T b, T c)
     }
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::normalf(T mean, T stddev)
+T RandomBase<RandomTraits>::normalf(T mean, T stddev)
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
     std::normal_distribution<T> dis{ mean, stddev };
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
-inline bool Random::yesNo() { return static_cast<bool>(Random::uniform<int>(0, 1)); }
+template <typename RandomTraits>
+inline bool RandomBase<RandomTraits>::yesNo() { return static_cast<bool>(uniform<int>(0, 1)); }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::probability()
+T RandomBase<RandomTraits>::probability()
 {
     static_assert(std::is_integral<T>::value, "Integral required.");
     std::uniform_int_distribution<T> dis(static_cast<T>(0), static_cast<T>(100));
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::probabilityf()
+T RandomBase<RandomTraits>::probabilityf()
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
     std::uniform_real_distribution<T> dis(
         static_cast<T>(0.f), static_cast<T>(std::nextafter(1.f, std::numeric_limits<T>::max())));
     // nextafter used to simulate closed interval
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniform()
+T RandomBase<RandomTraits>::uniform()
 {
     static_assert(std::is_integral<T>::value, "Integral required.");
 
     std::uniform_int_distribution<T> dis;
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniform(T to)
+T RandomBase<RandomTraits>::uniform(T to)
 {
     static_assert(std::is_integral<T>::value, "Integral required.");
 
     std::uniform_int_distribution<T> dis(static_cast<T>(0), to);
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniform(T from, T to)
+T RandomBase<RandomTraits>::uniform(T from, T to)
 {
     static_assert(std::is_integral<T>::value, "Integral required.");
 
     std::uniform_int_distribution<T> dis(from, to);
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniformf()
+T RandomBase<RandomTraits>::uniformf()
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
 
@@ -136,45 +160,49 @@ T Random::uniformf()
     // INFO: Don't add nextafter, correct range is (0, 1]
     //
     std::uniform_real_distribution<T> dis(static_cast<T>(0), static_cast<T>(1));
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniformf(T to)
+T RandomBase<RandomTraits>::uniformf(T to)
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
 
     std::uniform_real_distribution<T> dis(
         static_cast<T>(0), static_cast<T>(std::nextafter(to, std::numeric_limits<T>::max())));
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename T>
-T Random::uniformf(T from, T to)
+T RandomBase<RandomTraits>::uniformf(T from, T to)
 {
     static_assert(std::is_floating_point<T>::value, "Floating point required.");
 
     std::uniform_real_distribution<T> dis(from, to);
-    return dis(randomDevice());
+    return dis(RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <class RandomAccessIterator>
-void Random::shuffle(RandomAccessIterator first, RandomAccessIterator last)
+void RandomBase<RandomTraits>::shuffle(RandomAccessIterator first, RandomAccessIterator last)
 {
     //
     // std::random_shuffle is depricated and removed in C++17, don't use it
     //
     // https://meetingcpp.com/blog/items/stdrandom_shuffle-is-deprecated.html
     //
-    std::shuffle(first, last, randomDevice());
+    std::shuffle(first, last, RandomTraits::generator());
 }
 
+template <typename RandomTraits>
 template <typename C>
-typename C::value_type Random::weightedUniformFrom(const std::vector<float>& weights, const C& collection)
+typename C::value_type RandomBase<RandomTraits>::weightedUniformFrom(const std::vector<float>& weights, const C& collection)
 {
     std::discrete_distribution<size_t> dis(weights.begin(), weights.end());
 
-    auto offset = dis(randomDevice());
+    auto offset = dis(RandomTraits::generator());
 
     auto it = collection.begin();
     std::advance(it, static_cast<long>(offset));
@@ -188,8 +216,9 @@ typename C::value_type Random::weightedUniformFrom(const std::vector<float>& wei
 // Don't change type 'C::value_type' to reference type, because we can
 // pick item from collection on rvalue object e.g.'uniformFrom(getMyFancyVector())'
 //
+template <typename RandomTraits>
 template <typename C>
-typename C::value_type Random::uniformFrom(const C& collection)
+typename C::value_type RandomBase<RandomTraits>::uniformFrom(const C& collection)
 {
     //
     // TODO: uncomment this line when C++17 would be available
@@ -210,12 +239,8 @@ typename C::value_type Random::uniformFrom(const C& collection)
 }
 
 //
-// if you want one random device for all, please move this function to .cpp file
+// use types below
 //
-inline std::random_device& Random::randomDevice()
-{
-    static std::random_device s_device;
-    return s_device;
-}
+using Random = RandomBase<FastRandomTraits>;
 
 #endif // RANDOM_HPP
